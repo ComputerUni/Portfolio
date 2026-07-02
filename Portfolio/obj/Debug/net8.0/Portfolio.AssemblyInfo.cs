4745ca1e6e394be369b9dcee6ecd79910993b762
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-07-02 22:28:49 +0300

    updated: Project

--- Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b3a855f53cb628aacbb2945bb05db4af5b8f5bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77f147b414dd0b7298b32667e4d9bc385762992d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
